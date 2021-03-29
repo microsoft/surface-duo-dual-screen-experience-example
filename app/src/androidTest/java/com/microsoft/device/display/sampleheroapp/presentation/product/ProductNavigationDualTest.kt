9b58687b6759dec47bf294575b66b8ce0a2fb350
EUROPE\bimiron <bimiron@microsoft.com>
2021-03-29 14:28:13 +0300

    Created some navigation tests for the product feature, left some commented lines for future integrations or SDK issues

---
+++ app/src/androidTest/java/com/microsoft/device/display/sampleheroapp/presentation/product/ProductNavigationDualTest.kt
@@ -0,0 +1,107 @@
+/*
+ *
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ *
+ */
+
+package com.microsoft.device.display.sampleheroapp.presentation.product
+
+import androidx.test.rule.ActivityTestRule
+import com.microsoft.device.display.sampleheroapp.presentation.MainActivity
+import com.microsoft.device.display.sampleheroapp.util.setOrientationRight
+import com.microsoft.device.display.sampleheroapp.util.switchFromSingleToDualScreen
+import com.microsoft.device.display.sampleheroapp.util.unfreezeRotation
+import com.microsoft.device.dualscreen.ScreenManagerProvider
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+
+@HiltAndroidTest
+class ProductNavigationDualTest {
+
+    private val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @get:Rule
+    var ruleChain: RuleChain =
+        RuleChain.outerRule(HiltAndroidRule(this)).around(activityRule)
+
+    @After
+    fun resetOrientation() {
+        unfreezeRotation()
+        ScreenManagerProvider.getScreenManager().clear()
+    }
+
+    @Test
+    fun openProductsInDualPortraitMode() {
+        switchFromSingleToDualScreen()
+
+        openProductsTab()
+
+        checkProductList(PRODUCT_FIRST_POSITION, product)
+        checkProductDetails(product)
+        checkCustomizeButton()
+    }
+
+    @Test
+    fun openProductsInDualLandscapeMode() {
+        switchFromSingleToDualScreen()
+        setOrientationRight()
+
+        openProductsTab()
+
+        checkProductList(PRODUCT_FIRST_POSITION, product)
+        checkProductDetails(product)
+        checkCustomizeButton()
+    }
+
+    @Test
+    fun openCustomizeInDualPortraitMode() {
+        switchFromSingleToDualScreen()
+
+        openProductsTab()
+
+        checkProductList(PRODUCT_FIRST_POSITION, product)
+        checkProductDetails(product)
+        checkCustomizeButton()
+
+        clickOnCustomizeButton()
+
+        checkCustomizeControl()
+        checkCustomizeImagePortrait()
+        // checkCustomizeDetails(product)
+        // checkCustomizeDetailsImagePortrait()
+
+        goBack()
+
+        checkProductList(PRODUCT_FIRST_POSITION, product)
+        checkProductDetails(product)
+    }
+
+    @Test
+    fun openCustomizeInDualLandscapeMode() {
+        switchFromSingleToDualScreen()
+        setOrientationRight()
+
+        openProductsTab()
+
+        checkProductList(PRODUCT_FIRST_POSITION, product)
+        checkProductDetails(product)
+        checkCustomizeButton()
+
+        clickOnCustomizeButton()
+
+        checkCustomizeControl()
+        checkCustomizeImageLandscape()
+        // checkCustomizeDetails(product)
+        // checkCustomizeDetailsImageLandscape()
+
+        goBack()
+
+        checkProductList(PRODUCT_FIRST_POSITION, product)
+        checkProductDetails(product)
+    }
+}
