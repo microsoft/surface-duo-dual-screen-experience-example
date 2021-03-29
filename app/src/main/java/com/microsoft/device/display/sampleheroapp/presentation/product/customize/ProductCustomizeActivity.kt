4bcd305739e2f231fdf5075759baf3b0c67a3cf3
EUROPE\bimiron <bimiron@microsoft.com>
2021-03-29 14:26:45 +0300

    Created the customize activity and fragments according to design, opened them from the ProductDetailsFragment's Customize button and introduced the lottie library for the Place Order button animation

---
+++ app/src/main/java/com/microsoft/device/display/sampleheroapp/presentation/product/customize/ProductCustomizeActivity.kt
@@ -0,0 +1,94 @@
+/*
+ *
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ *
+ */
+
+package com.microsoft.device.display.sampleheroapp.presentation.product.customize
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.device.display.sampleheroapp.R
+import com.microsoft.device.display.sampleheroapp.presentation.product.customize.ProductCustomizeViewModel.Companion.SELECTED_DEFAULT_VALUE
+import com.microsoft.device.display.sampleheroapp.presentation.product.customize.ProductCustomizeViewModel.Companion.SELECTED_PRODUCT_ID
+import com.microsoft.device.display.sampleheroapp.presentation.util.RotationViewModel
+import com.microsoft.device.display.sampleheroapp.presentation.util.tutorial.TutorialViewModel
+import com.microsoft.device.dualscreen.ScreenInfo
+import com.microsoft.device.dualscreen.ScreenInfoListener
+import com.microsoft.device.dualscreen.ScreenManagerProvider
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProductCustomizeActivity : AppCompatActivity(), ScreenInfoListener {
+
+    private val viewModel: ProductCustomizeViewModel by viewModels()
+    private val tutorialViewModel: TutorialViewModel by viewModels()
+    private val rotationViewModel: RotationViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_customize)
+        intent.getLongExtra(SELECTED_PRODUCT_ID, SELECTED_DEFAULT_VALUE)
+            .takeIf { it != SELECTED_DEFAULT_VALUE }
+            ?.let { viewModel.initSelectedProduct(it) }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        ScreenManagerProvider.getScreenManager().addScreenInfoListener(this)
+    }
+
+    override fun onScreenInfoChanged(screenInfo: ScreenInfo) {
+        rotationViewModel.currentRotation.value = screenInfo.getScreenRotation()
+
+        if (screenInfo.isDualMode()) {
+            setupDualScreenFragments()
+            tutorialViewModel.onDualMode()
+        } else {
+            setupSingleScreenFragment()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        ScreenManagerProvider.getScreenManager().removeScreenInfoListener(this)
+    }
+
+    private fun setupSingleScreenFragment() {
+        if (supportFragmentManager.findFragmentByTag(FRAGMENT_SINGLE_SCREEN) == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.first_container_id, ProductCustomizeFragment(), FRAGMENT_SINGLE_SCREEN)
+                .commit()
+        }
+    }
+
+    private fun setupDualScreenFragments() {
+        if (supportFragmentManager.findFragmentByTag(FRAGMENT_DUAL_START) == null &&
+            supportFragmentManager.findFragmentByTag(FRAGMENT_DUAL_END) == null
+        ) {
+            supportFragmentManager.beginTransaction()
+                .replace(
+                    R.id.first_container_id,
+                    ProductCustomizeFragment(),
+                    FRAGMENT_DUAL_START
+                )
+                .commit()
+
+            supportFragmentManager.beginTransaction()
+                .replace(
+                    R.id.second_container_id,
+                    ProductCustomizeDetailsFragment(),
+                    FRAGMENT_DUAL_END
+                )
+                .commit()
+        }
+    }
+
+    companion object {
+        private const val FRAGMENT_DUAL_START = "ProductCustomizeFragment"
+        private const val FRAGMENT_DUAL_END = "ProductCustomizeDetailsFragment"
+        private const val FRAGMENT_SINGLE_SCREEN = "ProductCustomizeFragmentSingle"
+    }
+}
--- app/src/main/java/com/microsoft/device/display/sampleheroapp/presentation/product/customize/ProductCustomizeDetailsFragment.kt
+++ app/src/main/java/com/microsoft/device/display/sampleheroapp/presentation/product/customize/ProductCustomizeDetailsFragment.kt
@@ -5,9 +5,8 @@
  *
  */
 
-package com.microsoft.device.display.sampleheroapp.presentation.product.details
+package com.microsoft.device.display.sampleheroapp.presentation.product.customize
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,29 +14,27 @@ import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.airbnb.lottie.LottieAnimationView
 import com.microsoft.device.display.sampleheroapp.R
-import com.microsoft.device.display.sampleheroapp.databinding.FragmentProductDetailsBinding
-import com.microsoft.device.display.sampleheroapp.presentation.product.ProductViewModel
-import com.microsoft.device.display.sampleheroapp.presentation.product.customize.ProductCustomizeActivity
-import com.microsoft.device.display.sampleheroapp.presentation.product.customize.ProductCustomizeViewModel.Companion.SELECTED_PRODUCT_ID
+import com.microsoft.device.display.sampleheroapp.databinding.FragmentProductCustomizeDetailsBinding
 import com.microsoft.device.display.sampleheroapp.presentation.product.util.getProductDrawable
 import com.microsoft.device.display.sampleheroapp.presentation.util.RotationViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ProductDetailsFragment : Fragment() {
+class ProductCustomizeDetailsFragment : Fragment() {
 
-    private val viewModel: ProductViewModel by activityViewModels()
+    private val viewModel: ProductCustomizeViewModel by activityViewModels()
     private val rotationViewModel: RotationViewModel by activityViewModels()
 
-    private var binding: FragmentProductDetailsBinding? = null
+    private var binding: FragmentProductCustomizeDetailsBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentProductDetailsBinding.inflate(inflater, container, false)
+        binding = FragmentProductCustomizeDetailsBinding.inflate(inflater, container, false)
         binding?.viewModel = viewModel
         binding?.rotationViewModel = rotationViewModel
         binding?.lifecycleOwner = this
@@ -64,30 +61,21 @@ class ProductDetailsFragment : Fragment() {
             }
         )
 
-        binding?.productDetailsImage?.apply {
-            viewModel.selectedProduct.value?.let {
-                if (it.colorId != null && it.bodyShape != null) {
-                    setImageDrawable(
-                        ContextCompat.getDrawable(
-                            requireContext(),
-                            getProductDrawable(it.colorId, it.bodyShape)
-                        )
+        binding?.productDetailsCustomizePlaceOrder?.setOnClickListener {
+            (it as LottieAnimationView).playAnimation()
+        }
+
+        viewModel.selectedBodyColor.observe(
+            viewLifecycleOwner,
+            {
+                val shape = viewModel.selectedBodyShape.value
+                if (it != null && shape != null) {
+                    binding?.productDetailsImage?.setImageDrawable(
+                        ContextCompat.getDrawable(requireContext(), getProductDrawable(it, shape))
                     )
                 }
             }
-        }
-
-        viewModel.productList.observe(viewLifecycleOwner, { viewModel.selectFirstProduct() })
-
-        binding?.productDetailsCustomizeButton?.setOnClickListener {
-            startActivity(
-                Intent(requireContext(), ProductCustomizeActivity::class.java).apply {
-                    viewModel.selectedProduct.value?.productId?.let {
-                        putExtra(SELECTED_PRODUCT_ID, it)
-                    }
-                }
-            )
-        }
+        )
     }
 
     override fun onDestroyView() {
