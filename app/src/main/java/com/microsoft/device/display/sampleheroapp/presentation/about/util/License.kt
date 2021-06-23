7eb0a4db27c97413d87d722b4dd50086c7856648
EUROPE\bimiron <bimiron@microsoft.com>
2021-06-23 10:16:40 +0300

    Created OSS licenses section of the About page

---
+++ app/src/main/java/com/microsoft/device/display/sampleheroapp/presentation/about/util/License.kt
@@ -0,0 +1,10 @@
+/*
+ *
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ *
+ */
+
+package com.microsoft.device.display.sampleheroapp.presentation.about.util
+
+data class License(val name: String, val license: String, val url: String)
